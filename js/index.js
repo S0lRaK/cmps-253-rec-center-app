ffe2c9a95370e4a54bf9b9733bba0d829fa44d71
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-03-31 20:14:50 -0600

    Current date format

--- js/index.js
+++ js/index.js
@@ -1,11 +1,45 @@
-onload = function()
-{
-  var dt = new Date();
-document.getElementById("date").innerHTML = (("0"+dt.getDate()).slice(-2)) +"/"+ (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+"use strict";
 
-}
+window.onload = setCurrentDateTime;
 
-function moreNews(){
-  let displayRow = document.getElementById("newsR3");
-  displayRow.style = true;
+function setCurrentDateTime() {
+  let months = new Array(
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	);
+  let now = new Date();
+  let day = now.getDate();
+	let month = now.getMonth();
+	let currentMonth = months[month];
+  let year = now.getFullYear();
+  let time = now.toLocaleTimeString();
+	now = currentMonth + " " + day + ", " + year + " " + time;
+	document.getElementById("date").innerHTML = now;
+
+
+	/* document.getElementById("date").innerHTML =
+		("0" + dt.getDate()).slice(-2) +
+		"/" +
+		("0" + (dt.getMonth() + 1)).slice(-2) +
+		"/" +
+		dt.getFullYear() +
+		" " +
+		("0" + dt.getHours()).slice(-2) +
+		":" +
+		("0" + dt.getMinutes()).slice(-2); */
+};
+
+function moreNews() {
+	let displayRow = document.getElementById("newsR3");
+	displayRow.style = true;
 }
