e7a9d18c688be849b2799e55af3f1a7047ba7a33
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-03-31 19:42:25 -0600

    Home page fix

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function openPop(){
+    document.getElementById("login").style.display = "block";
+}
+function closePop(){
+    document.getElementById("login").style.display = "none";
+}
--- login.js
+++
@@ -1,6 +0,0 @@
-function openPop(){
-    document.getElementById("loginPop").style.display = "block";
-}
-function closePop(){
-    document.getElementById("loginPop").style.display = "none";
-}
