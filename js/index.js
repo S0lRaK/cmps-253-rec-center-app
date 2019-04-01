851203f4d9cdaceef5d1303d5dfa9635c30fd72e
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-03-31 21:02:27 -0600

    Navbar with logo

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 window.onload = setCurrentDateTime;
 
 function setCurrentDateTime() {
-  let months = new Array(
+	let months = new Array(
 		"January",
 		"February",
 		"March",
@@ -17,27 +17,15 @@ function setCurrentDateTime() {
 		"November",
 		"December"
 	);
-  let now = new Date();
-  let day = now.getDate();
+	let now = new Date();
+	let day = now.getDate();
 	let month = now.getMonth();
 	let currentMonth = months[month];
-  let year = now.getFullYear();
-  let time = now.toLocaleTimeString();
+	let year = now.getFullYear();
+	let time = now.toLocaleTimeString();
 	now = currentMonth + " " + day + ", " + year + " " + time;
 	document.getElementById("date").innerHTML = now;
-
-
-	/* document.getElementById("date").innerHTML =
-		("0" + dt.getDate()).slice(-2) +
-		"/" +
-		("0" + (dt.getMonth() + 1)).slice(-2) +
-		"/" +
-		dt.getFullYear() +
-		" " +
-		("0" + dt.getHours()).slice(-2) +
-		":" +
-		("0" + dt.getMinutes()).slice(-2); */
-};
+}
 
 function moreNews() {
 	let displayRow = document.getElementById("newsR3");
