5ebc3cacf3fab10f2bc070128a2fa81dd08bcc6c
Carlos J. Garcia Carmona <carlosj.gc989@gmail.com>
2019-04-09 23:43:55 -0600

    Login/Sign up pop up

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
-function openPop(){
-    document.getElementById("login").style.display = "block";
+"use strict";
+
+function togglePopUp() {
+	const profileElement = document.getElementById("profile");
+
+	profileElement.hasAttribute("hidden")
+		? profileElement.removeAttribute("hidden")
+		: profileElement.setAttribute("hidden", "");
 }
-function closePop(){
-    document.getElementById("login").style.display = "none";
+
+function toggleOption(divId) {
+    stopEventPropagation;
+    
+	const options = document.querySelectorAll("#profile .option");
+
+	options.forEach(option => {
+		option.id == divId
+			? option.classList.remove("hidden")
+			: option.classList.add("hidden");
+	});
+}
+
+function stopEventPropagation() {
+	if (!e) var e = window.event;
+	e.cancelBubble = true;
+	if (e.stopPropagation) e.stopPropagation();
 }
